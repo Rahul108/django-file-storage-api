5f8c52589ca00a3599b723425834143ee7821984
rahul <rahul@surecash.net>
2022-06-12 17:18:37 +0600

    Enabling Partial Saving

--- store_media/serializers.py
+++ store_media/serializers.py
@@ -5,16 +5,18 @@ import mimetypes, magic
 class MediaStorageSerailizer(serializers.Serializer):
     file = serializers.ListField()
     
-    def validate_file(self, file):
-        res = []
-        for elm in file:
+    def validate(self, data):
+        res = {
+            'files': [],
+            'errors': []
+        }
+        for elm in data['file']:
             if elm:
                 temp_path = elm.temporary_file_path()
                 validate_file = magic.from_file(temp_path, mime=True)
                 get_file_mimetype = mimetypes.guess_type(elm.name)
                 if validate_file == get_file_mimetype[0]:
-                    res.append(elm)
+                    res['files'].append(elm)
                 else:
-                    serializers.ValidationError(f'Invalid/Corrupted-File for {elm.name}')
-        
+                    res['errors'].append(f'Invalid/Corrupted-File for {elm.name}')
         return res
--- store_media/services.py
+++ store_media/services.py
@@ -7,14 +7,17 @@ class MediaFileSaveService:
         self.validated_data = validated_data
 
     def save_media_files(self):
-        res = []
+        res = {
+            'files_url': [],
+            'errors': self.validated_data['errors']
+        }
         
-        for file in self.validated_data['file']:
+        for file in self.validated_data['files']:
             file_type = mimetypes.guess_type(file.name)
             extension = mimetypes.guess_extension(file_type[0])
             new_file_name = str(format(time.time() * 1000000000, ".0f")) + extension
             file_name = default_storage.save(new_file_name, file)
             file_url = default_storage.url(file_name)
             result = settings.BASE_URL+file_url
-            res.append(result)
+            res['files_url'].append(result)
         return res
